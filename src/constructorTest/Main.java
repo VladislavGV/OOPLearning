8bf83a7a5c8ab4263471ee3babaaf41d8e2b4877
VladislavGV <0377722@mail.ru>
2022-04-26 11:21:36 +0300

    Hide Person

--- src/constructorTest/Main.java
+++ src/constructorTest/Main.java
@@ -1,37 +1,37 @@
 package constructorTest;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        Person person = new Person("Sergey", 28, "Proletarskaya 10", "88005553535");
-        Person.getCounter();
-        Fraction fraction = new Fraction(1, 10);
-    }
-}
+//    public static void main(String[] args) throws Exception {
+//        Person person = new Person("Sergey", 28, "Proletarskaya 10", "88005553535");
+//        Person.getCounter();
+//        Fraction fraction = new Fraction(1, 10);
+//    }
+//}
 
-class Person{
-    int id;
-    private static String name;
-    private int age;
-    private static int counter = 0;
-
-    public int getId() {
-        return id;
-    }
-
-    public static int getCounter() {
-        return counter;
-    }
-    //    public String address;
-//    private String phone;
-
-    static {
-        System.out.println(counter);
-    }
-
-    public Person(String name, int age, String adress, String phone) {
-        id = counter++;
-        this.name = name;
-        this.age = age;
+//class Person {
+//    int id;
+//    private static String name;
+//    private int age;
+//    private static int counter = 0;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public static int getCounter() {
+//        return counter;
+//    }
+//    //    public String address;
+////    private String phone;
+//
+//    static {
+//        System.out.println(counter);
+//    }
+//
+//    public Person(String name, int age, String adress, String phone) {
+//        id = counter++;
+//        this.name = name;
+//        this.age = age;
 
 
 //        this.address = address;
@@ -54,37 +54,43 @@ class Person{
 //    }
 
 
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(int age) {
+//        if (age > 0 && age < 110)
+//            this.age = age;
+//    }
+//}
 
-    public int getAge() {
-        return age;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        if(age >0 && age < 110)
-        this.age = age;
-    }
-}
-
-
-class Fraction{
+class Fraction {
     int numerator;
     int denominator = 1;
 
-    public Fraction(int numerator, int denominator){
-        if(denominator == 0){
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
             System.out.println("Denominator can't be zero");
             return;
         }
         this.numerator = numerator;
         this.denominator = denominator;
+    }
 
+    public Fraction add1ToNumerator() {
+        this.numerator++;
+        return this;
     }
 
 }
+
+
